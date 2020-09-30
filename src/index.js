8f8b2ab3c5bff1c39474a6f6a75355321b55a685
Edgar Zea <cesszea@icloud.com>
2020-09-29 21:48:48 -0500

    Service worker added

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import CalendarApp from "./CalendarApp";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/styles.css";
 import "fontsource-roboto";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<CalendarApp />, document.getElementById("root"));
+
+serviceWorker.register();
